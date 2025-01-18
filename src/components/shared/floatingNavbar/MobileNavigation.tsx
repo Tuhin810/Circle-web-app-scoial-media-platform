d18c5928d6ed91719c8bb81c16b01e8b5e87f1a6
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-01-19 04:55:10 +0530

    fix: increase height of SVG in MobileNavigation component

--- src/components/shared/floatingNavbar/MobileNavigation.tsx
+++ src/components/shared/floatingNavbar/MobileNavigation.tsx
@@ -91,7 +91,7 @@ const MobileNavigation = () => {
       </ul>
 
       <svg
-        className="h-10"
+        className="h-20"
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
         id="filter-svg"
